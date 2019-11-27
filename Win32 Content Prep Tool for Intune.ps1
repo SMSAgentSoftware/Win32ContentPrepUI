71452dd12d4e1b4540ac8e449062f12b6c9bb4f6
Trevor Jones <SMSAgentSoftware@users.noreply.github.com>
2019-11-27 11:46:21 +0000

    Add files via upload

---
+++ Win32 Content Prep Tool for Intune.ps1
@@ -0,0 +1,81 @@
+﻿###############################################
+##                                           ##
+##     Win32 Content Prep Tool for Intune    ##
+##                                           ##
+## Author:      Trevor Jones                 ##
+## Blog:        smsagent.blog                ##
+##                                           ##
+###############################################
+
+
+# Set the location we are running from
+$Source = $PSScriptRoot
+
+# Load the required assemblies
+Add-Type -AssemblyName PresentationFramework,PresentationCore,WindowsBase,System.Windows.Forms
+Add-Type -Path "$Source\bin\System.Windows.Interactivity.dll"
+Add-Type -Path "$Source\bin\ControlzEx.dll"
+Add-Type -Path "$Source\bin\MahApps.Metro.dll"
+
+# Load the main window XAML code
+[XML]$Xaml = [System.IO.File]::ReadAllLines("$Source\Xaml\App.xaml") 
+
+# Create a synchronized hash table and add the WPF window and its named elements to it
+$UI = [System.Collections.Hashtable]::Synchronized(@{})
+$UI.Window = [Windows.Markup.XamlReader]::Load((New-Object -TypeName System.Xml.XmlNodeReader -ArgumentList $xaml))
+$xaml.SelectNodes("//*[@*[contains(translate(name(.),'n','N'),'Name')]]") | 
+    ForEach-Object -Process {
+        $UI.$($_.Name) = $UI.Window.FindName($_.Name)
+    }
+
+# Set the window icon from a file
+$UI.Window.Icon = "$Source\bin\cloud.ico"
+
+# Load in the other code libraries.
+. "$Source\bin\EventLibrary.ps1"
+
+# OC for data binding source
+$UI.DataSource = New-Object System.Collections.ObjectModel.ObservableCollection[Object]
+$UI.DataSource.Add("Ready") # [0] Status bar text
+$UI.DataSource.Add("Black") # [1] Status bar text colour
+
+# Set the datacontext of the window to the OC for databinding
+$UI.Window.DataContext = $UI.DataSource
+
+# Create reg keys if needed and set the IntuneAppPath
+If (!(Test-Path HKCU:\Software\SMSAgent))
+{
+    $null = New-Item -Path HKCU:\Software -Name SMSAgent
+}
+If (!(Test-Path HKCU:\Software\SMSAgent\Win32ContentPrepUI))
+{
+    $null = New-Item -Path HKCU:\Software\SMSAgent -Name Win32ContentPrepUI 
+}
+If (!(Get-ItemProperty -Path HKCU:\Software\SMSAgent\Win32ContentPrepUI -Name IntuneAppPath -ErrorAction SilentlyContinue))
+{
+    $null = New-ItemProperty -Path HKCU:\Software\SMSAgent\Win32ContentPrepUI -Name IntuneAppPath -PropertyType String
+}
+Else
+{
+    $UI.IntuneAppPath.Text = Get-ItemProperty -Path HKCU:\Software\SMSAgent\Win32ContentPrepUI -Name IntuneAppPath | Select -ExpandProperty IntuneAppPath
+}
+
+# Display the main window
+# If code is running in ISE, use ShowDialog()...
+if ($psISE)
+{
+    $null = $UI.window.Dispatcher.InvokeAsync{$UI.window.ShowDialog()}.Wait()
+}
+# ...otherwise run as an application
+Else
+{
+    # Hide the PowerShell console window
+    $windowcode = '[DllImport("user32.dll")] public static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);'
+    $asyncwindow = Add-Type -MemberDefinition $windowcode -Name Win32ShowWindowAsync -Namespace Win32Functions -PassThru
+    $null = $asyncwindow::ShowWindowAsync((Get-Process -PID $pid).MainWindowHandle, 0)
+    
+    # Run the main window in an application
+    $app = New-Object -TypeName Windows.Application
+    $app.Properties
+    $app.Run($UI.Window)
+}
---
+++ bin/EventLibrary.ps1
@@ -0,0 +1,186 @@
+﻿#############################
+##                         ##
+## Defines event handlers  ##
+##                         ##
+#############################
+
+# Bring the main window to the front once loaded
+$UI.Window.Add_Loaded({
+    $This.Activate()
+})
+
+# OpenFileDialog for IntuneWinAppUtil
+$UI.BrowseIntuneAppPath.Add_Click({
+
+    # Use the windows forms openfiledialog window
+    $OpenFileDialog = New-Object System.Windows.Forms.OpenFileDialog
+    $OpenFileDialog.initialDirectory = $env:SystemDrive
+    $OpenFileDialog.filter = "IntuneWinAppUtil (IntuneWinAppUtil.exe)| IntuneWinAppUtil.exe"
+    $Response = $OpenFileDialog.ShowDialog()
+
+    # If not cancelled
+    If ($Response -eq "OK")
+    {
+        # Select filename and full file path
+        $UI.IntuneAppPath.Text = $OpenFileDialog.FileName
+    }
+
+})
+
+# FolderBrowserDialog for Setup folder
+$UI.BrowseSetupFolder.Add_Click({
+
+    # Use the windows forms openfiledialog window
+    $FolderBrowserDialog = New-Object System.Windows.Forms.FolderBrowserDialog
+    $Response = $FolderBrowserDialog.ShowDialog()
+
+    # If not cancelled
+    If ($Response -eq "OK")
+    {
+        # Select filename and full file path
+        $UI.TextSetupFolder.Text = $FolderBrowserDialog.SelectedPath
+    }
+
+})
+
+# OpenFileDialog for Setup file
+$UI.BrowseSetupFile.Add_Click({
+
+    # Use the windows forms openfiledialog window
+    $OpenFileDialog = New-Object System.Windows.Forms.OpenFileDialog
+    $OpenFileDialog.initialDirectory = $UI.TextSetupFolder.Text
+    $Response = $OpenFileDialog.ShowDialog()
+
+    # If not cancelled
+    If ($Response -eq "OK")
+    {
+        # Select filename and full file path
+        $UI.TextSetupFile.Text = $OpenFileDialog.FileName
+    }
+
+})
+
+# FolderBrowserDialog for Output folder
+$UI.BrowseOutputFolder.Add_Click({
+
+    # Use the windows forms openfiledialog window
+    $FolderBrowserDialog = New-Object System.Windows.Forms.FolderBrowserDialog
+    $Response = $FolderBrowserDialog.ShowDialog()
+
+    # If not cancelled
+    If ($Response -eq "OK")
+    {
+        # Select filename and full file path
+        $UI.TextOutputFolder.Text = $FolderBrowserDialog.SelectedPath
+    }
+
+})
+
+# Run the IntuneWinAppUtil.exe
+$UI.Package.Add_Click({
+   
+    $IntuneWinAppUtilPath = $UI.IntuneAppPath.Text
+    $SetupFolder = $UI.TextSetupFolder.Text
+    $SetupFile = $UI.TextSetupFile.Text
+    $OutputFolder = $UI.TextOutputFolder.Text
+
+    If (!(Test-Path $IntuneWinAppUtilPath))
+    {
+        $UI.DataSource[0] = "Error: '$IntuneWinAppUtilPath' is not a valid location'"
+        $UI.DataSource[1] = "Red"
+        Return
+    }
+    If (!(Test-Path $SetupFolder))
+    {
+        $UI.DataSource[0] = "Error: '$SetupFolder' is not a valid location'"
+        $UI.DataSource[1] = "Red"
+        Return
+    }
+    If (!(Test-Path $SetupFile))
+    {
+        $UI.DataSource[0] = "Error: '$SetupFile' is not a valid location'"
+        $UI.DataSource[1] = "Red"
+        Return
+    }
+    If (!(Test-Path $OutputFolder))
+    {
+        $UI.DataSource[0] = "Error: '$OutputFolder' is not a valid location'"
+        $UI.DataSource[1] = "Red"
+        Return
+    }
+
+    $UI.DataSource[0] = "Processing..."
+    $UI.DataSource[1] = "Blue"
+
+    Try
+    {
+        $Process = Start-Process -FilePath $IntuneWinAppUtilPath -ArgumentList "-c ""$SetupFolder"" -s ""$SetupFile"" -o ""$OutputFolder"" -q" -Wait -PassThru -ErrorAction Stop
+        If ($Process.ExitCode -eq 0)
+        {
+            $UI.DataSource[0] = "Process exited with code 0...Success!"
+            $UI.DataSource[1] = "Green"
+        }
+        Else
+        {
+            $UI.DataSource[0] = "Process exited with code $($Process.ExitCode)...Failed!"
+            $UI.DataSource[1] = "Red"
+        }
+        Invoke-Item $OutputFolder
+    }
+    Catch
+    {
+        $UI.DataSource[0] = "Error: Could not start '$IntuneWinAppUtilPath'!"
+        $UI.DataSource[1] = "Red"
+    }
+
+})
+
+# text changed events to enable / disable the Package button
+$UI.IntuneAppPath.Add_TextChanged({
+    If ($This.Text -ne "" -and $UI.TextSetupFolder.Text -ne "" -and $UI.TextSetupFile.Text -ne "" -and $UI.TextOutputFolder.Text -ne "")
+    {
+        $UI.Package.IsEnabled = "True"
+    }
+    Else
+    {
+        $UI.Package.IsEnabled = $False
+    }
+})
+
+$UI.TextSetupFolder.Add_TextChanged({
+    If ($This.Text -ne "" -and $UI.IntuneAppPath.Text -ne "" -and $UI.TextSetupFile.Text -ne "" -and $UI.TextOutputFolder.Text -ne "")
+    {
+        $UI.Package.IsEnabled = "True"
+    }
+    Else
+    {
+        $UI.Package.IsEnabled = $False
+    }
+})
+
+$UI.TextSetupFile.Add_TextChanged({
+    If ($This.Text -ne "" -and $UI.IntuneAppPath.Text -ne "" -and $UI.TextSetupFolder.Text -ne "" -and $UI.TextOutputFolder.Text -ne "")
+    {
+        $UI.Package.IsEnabled = "True"
+    }
+    Else
+    {
+        $UI.Package.IsEnabled = $False
+    }
+})
+
+$UI.TextOutputFolder.Add_TextChanged({
+    If ($This.Text -ne "" -and $UI.IntuneAppPath.Text -ne "" -and $UI.TextSetupFolder.Text -ne "" -and $UI.TextSetupFile.Text -ne "")
+    {
+        $UI.Package.IsEnabled = "True"
+    }
+    Else
+    {
+        $UI.Package.IsEnabled = $False
+    }
+})
+
+# Write the IntuneWinAppUtil path to registry for future retrieval
+$UI.Window.Add_Closing({
+    Set-ItemProperty -Path HKCU:\Software\SMSAgent\Win32ContentPrepUI -Name IntuneAppPath -Value $UI.IntuneAppPath.Text
+})
